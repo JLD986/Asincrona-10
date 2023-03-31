f331c7adbca40fb01c824cd5673a18be4948fc06
JLD986 <126208860+JLD986@users.noreply.github.com>
2023-03-31 17:33:16 -0600

    Update actividadAsincrona – Semana10(Grupo9).py

--- actividadAsincrona – Semana10(Grupo9).py
+++ actividadAsincrona – Semana10(Grupo9).py
@@ -16,11 +16,22 @@ while True:
     except ValueError:
         print("Error: el valor ingresado no es un número válido.")
 
+
 # Calcular el total de las compras del cliente
 total = sum(compras)
 
+if total>=500 and total<100:
+    descuento=((total*0.05)+total)
+    print(f"El total de las compras es: {descuento} con el 5% de descuento aplicado")
+
+elif total>=1000:
+    descuento=((total*0.1)+total)
+    print(f"El total de las compras es: {descuento} con el 10% de descuento aplicado")
+
+else:print(f"El total de las compras es: {total} sin descuento")
+
 # Imprimir el total de las compras del cliente
-print("El total de las compras del cliente es:", total)
+
 
 print('                                           FIN                                                              ')
 print('**********************************************************************************************************\n')
